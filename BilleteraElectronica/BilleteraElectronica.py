98434f7689857fab4eac4a8b04716825920a5e02
MauSalerno <mausalerno42@gmail.com>
2016-05-11 10:22:22 -0430

    Pruebas pin = 0, caracteres especiales castellano. Desarrollo software cumple pruebas.

--- BilleteraElectronica/BilleteraElectronica.py
+++ BilleteraElectronica/BilleteraElectronica.py
@@ -4,5 +4,31 @@ Created on 10/05/2016
 @author: PedrozaSalerno
 '''
 
-if __name__ == '__main__':
-    pass
+class Persona():
+    
+    def __init__(self, nombre, apellido, ci, pin):
+        if(type(nombre) is not type("safsdf")):
+            exit("El nombre debe ser un string")
+        elif(any(not(x.isalpha()) and x!="-" and  not(x.isspace()) for x in nombre)):
+            exit("Caracteres invalidos en el nombre.")
+        elif(type(apellido) is not type("safsdf")):
+            exit("El apellido debe ser un string")
+        elif(any(not(x.isalpha()) and x!="-" and  not(x.isspace()) for x in apellido)):
+            exit("Caracteres invalidos en el apellido.")
+        elif(type(ci) is not type("asd")):
+            exit("La cedula de identidad debe ser un string de la forma V-|E- seguida de numeros")
+        elif(len(ci)<3):
+            exit("La cedula de identidad debe ser un string de la forma V-|E- seguido de numeros")
+        elif(ci[:2] not in ["V-","E-"]):
+            exit("La cedula de identidad debe ser un string de la forma V-|E- seguido de numeros")
+        elif(any(not x.isdigit() for x in ci[2:])):
+            exit("La cedula de identidad debe ser un string de la forma V-|E- seguido de numeros")
+        elif(type(pin) is not int):
+            exit("El pin debe ser un numero entero mayor o igual que cero")
+        elif(pin<0):
+            exit("El pin debe ser un numero entereo mayor o igual que cero")
+            
+        self.nombre = nombre
+        self.apellido = apellido
+        self.ci = ci
+        self.pin = pin
